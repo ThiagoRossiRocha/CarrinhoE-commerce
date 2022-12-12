c0ab42078451b14cef0129d61ded7fdd1707f497
Thiago Rocha <thiago.rocha@fortesecurity.com.br>
2022-12-12 18:08:07 -0300

    Alterações no Front e Back-End

---
+++ api/src/index.ts
@@ -0,0 +1,59 @@
+import { Product } from "./entity/Product"
+import { AppDataSource } from "./data-source"
+
+AppDataSource.initialize().then(async () => {
+    /*
+    // cadastrar os produtos para o banco de dados...
+
+    const product = new Product()
+    product.image = "https://www.imagensempng.com.br/wp-content/uploads/2022/01/Coca-Cola-Lata-Png.png"
+    product.name = "Agua"
+    product.price = 4.00
+
+    await AppDataSource.manager.save(product)
+    console.log("Loading products from the database...")
+
+    const products = await AppDataSource.manager.find(Product)
+    console.log("Loaded products: ", products)
+
+    const allPriceProducts = AppDataSource.manager.find(Product);*/
+
+
+    
+    // Retornar os produtos do banco de dados...
+
+    const productRepository = AppDataSource.getRepository(Product)
+    const allProducts = await productRepository.find()
+    console.log("All products from the db: ", allProducts)
+
+
+    /*
+    const firstProduct = await productRepository.findOneBy({
+        id: 1,
+    })
+    console.log("First product from the db: ", firstProduct)
+
+    const productsAgua = await productRepository.findOneBy({
+        name: "Agua",
+    })
+    console.log("Me products = Agua from the db: ", productsAgua)
+
+    const allViewedProducts = await productRepository.findBy({ price: 4 })
+    console.log("All products from price for R$4,00 from the db : ", allViewedProducts)
+
+    const [product, productsCount] = await productRepository.findAndCount()
+    console.log("All products: ", product)
+    console.log("Products count: ", productsCount)
+    */
+
+    /*
+    //Atualizar os produtos no banco de dados
+    const productRepository = AppDataSource.getRepository(Product)
+    const productToUpdate = await productRepository.findOneBy({
+        id: 1,
+    })
+    productToUpdate.name = "teste nome alterado!!"
+    await productRepository.save(productToUpdate)
+    */
+
+}).catch(error => console.log(error))
---
+++ api/src/server.ts
@@ -0,0 +1,31 @@
+import * as http from 'http';
+import { Product } from "./entity/Product"
+import { AppDataSource } from "./data-source"
+import express from 'express';
+
+let allProducts : Product [];
+let allPriceProducts: any;
+
+const app = express();
+
+AppDataSource.initialize().then(async () => {
+
+    async function retornaDados(){
+        // Retornar os produtos do banco de dados...
+        const productRepository = AppDataSource.getRepository(Product)
+        allProducts = await productRepository.find()
+    }
+
+    retornaDados();
+    
+    app.get('/api/products', (request : any, response : any) => {
+        response.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+        response.status(200).send(allProducts);
+    });
+
+    // Servidor
+    const server = http.createServer(app);
+    const port = 8080;
+    server.listen(port, '0.0.0.0', () => console.log('O servidor está em execução http://localhost:8080/api/products'));
+
+}).catch(error => console.log(error))
